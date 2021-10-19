27e3740cc4d9bd96bbc04e9193d1934f08b66ba5
Svetoslav <68899725+Mastera21@users.noreply.github.com>
2021-10-19 11:28:00 +0300

    Adding files

---
+++ Game-Engine/build/CMakeFiles/gameEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gameEngine.dir/main.cpp.o"
+  "gameEngine.pdb"
+  "gameEngine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Game-Engine/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "SDL.h"
+
+
+int32_t main([[maybe_unused]]int32_t argc, [[maybe_unused]]char** argv){
+
+
+
+	return EXIT_SUCCESS;
+}
