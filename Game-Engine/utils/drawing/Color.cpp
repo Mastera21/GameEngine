21591a0011600b328f1b979f2d80dedf94bb9d8f
Svetoslav <68899725+Mastera21@users.noreply.github.com>
2021-10-23 15:53:04 +0300

    Implementing class Color

--- Game-Engine/utils/drawing/Color.cpp
+++ Game-Engine/utils/drawing/Color.cpp
@@ -8,4 +8,24 @@
 
 //Own components headers
 
+sd::Color::Color(const uint8_t red, const uint8_t green, const uint8_t blue, const uint8_t alpha){
+	rgba.r = red;
+	rgba.g = green;
+	rgba.b = blue;
+	rgba.a = alpha;
+}
 
+namespace Colors {
+const sd::Color RED(255, 0, 0, 255);
+const sd::Color GREEN(0, 255, 0, 255);
+const sd::Color BLUE(0, 0, 255, 255);
+const sd::Color BLACK(0, 0, 0, 255);
+const sd::Color WHITE(255, 255, 255, 255);
+const sd::Color GRAY(192, 192, 192, 255);
+const sd::Color CYAN(0, 255, 255, 255);
+const sd::Color MAGENTA(255, 0, 255, 255);
+const sd::Color YELLOW(255, 255, 0, 255);
+const sd::Color ORANGE(255, 128, 0, 255);
+const sd::Color PURPLE(128, 0, 128, 255);
+const sd::Color FULL_TRANSPARENT(0, 0, 0, 0);
+} //namespace Colors
