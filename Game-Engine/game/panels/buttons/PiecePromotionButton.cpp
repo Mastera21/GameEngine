5c88ce673b8ebbf6e143d84172a689939499bc0f
Svetoslav <68899725+Mastera21@users.noreply.github.com>
2021-11-15 12:54:41 +0200

    Adding PiecePromotionButton class

---
+++ Game-Engine/game/panels/buttons/PiecePromotionButton.cpp
@@ -0,0 +1,11 @@
+
+#include "game/panels/buttons/PiecePromotionButton.h"
+
+//C system headers
+
+//C++ system headers
+#include <iostream>
+
+//Other libraries headers
+
+//Own components headers
---
+++ Game-Engine/game/panels/buttons/PiecePromotionButton.h
@@ -0,0 +1,20 @@
+#ifndef GAME_PANELS_BUTTONS_PIECEPROMOTIONBUTTON_H_
+#define GAME_PANELS_BUTTONS_PIECEPROMOTIONBUTTON_H_
+
+//C system headers
+
+//C++ system headers
+#include <cstdint>
+//Other libraries headers
+
+//Own components headers
+
+//Forward declarations
+
+class PiecePromotionButton {
+public:
+	PiecePromotionButton();
+	virtual ~PiecePromotionButton();
+};
+
+#endif /* GAME_PANELS_BUTTONS_PIECEPROMOTIONBUTTON_H_ */
