13c22c4050a7e7617ee0ce6941c7098e2b002332
Svetoslav <68899725+Mastera21@users.noreply.github.com>
2021-11-15 12:54:13 +0200

    Adding PiecePromotionPanelCfg.h

---
+++ Game-Engine/game/panels/config/PiecePromotionPanelCfg.h
@@ -0,0 +1,19 @@
+#ifndef GAME_PANELS_CONFIG_PIECEPROMOTIONPANELCFG_H_
+#define GAME_PANELS_CONFIG_PIECEPROMOTIONPANELCFG_H_
+
+//C system headers
+
+//C++ system headers
+#include <cstdint>
+//Other libraries headers
+
+//Own components headers
+
+//Forward declarations
+
+struct PiecePromotionPanelCfg{
+
+};
+
+
+#endif /* GAME_PANELS_CONFIG_PIECEPROMOTIONPANELCFG_H_ */
