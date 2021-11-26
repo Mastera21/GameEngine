cb0fc69ee594637525055c7d40307f848d8d866c
Svetoslav <68899725+Mastera21@users.noreply.github.com>
2021-11-26 21:58:26 +0200

    Update in linux files

--- Game-Engine/build/CMakeFiles/gameEngine.dir/DependInfo.cmake
+++ Game-Engine/build/CMakeFiles/gameEngine.dir/DependInfo.cmake
@@ -1,43 +1,48 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/application/Application.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/application/Application.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/engine/Engine.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/engine/Engine.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/engine/EngineConfigLoader.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/engine/EngineConfigLoader.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/Game.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/Game.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/board/GameBoard.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/board/GameBoard.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/board/MoveSelector.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/board/MoveSelector.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/board/boardAnim/GameBoardAnim.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/board/boardAnim/GameBoardAnim.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/logic/GameLogic.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/logic/GameLogic.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/movesPlayersId/MovePlayersId.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/movesPlayersId/MovePlayersId.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/panels/PiecePromotionPanel.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/panels/PiecePromotionPanel.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/panels/buttons/PiecePromotionButton.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/panels/buttons/PiecePromotionButton.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/PieceHandler.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/PieceHandler.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/PieceHandlerPopulator.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/PieceHandlerPopulator.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/types/Bishop.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/types/Bishop.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/types/ChessPiece.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/types/ChessPiece.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/types/King.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/types/King.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/types/Knight.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/types/Knight.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/types/Pawn.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/types/Pawn.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/types/Queen.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/types/Queen.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/types/Rook.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/types/Rook.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/pieces/types/UnfinishedPiece.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/pieces/types/UnfinishedPiece.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/game/utils/BoardUtils.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/game/utils/BoardUtils.cpp.o"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/main.cpp" "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/CMakeFiles/gameEngine.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/application/Application.cpp" "CMakeFiles/gameEngine.dir/application/Application.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/application/Application.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/engine/Engine.cpp" "CMakeFiles/gameEngine.dir/engine/Engine.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/engine/Engine.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/engine/EngineConfigLoader.cpp" "CMakeFiles/gameEngine.dir/engine/EngineConfigLoader.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/engine/EngineConfigLoader.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/Game.cpp" "CMakeFiles/gameEngine.dir/game/Game.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/Game.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/board/GameBoard.cpp" "CMakeFiles/gameEngine.dir/game/board/GameBoard.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/board/GameBoard.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/board/MoveSelector.cpp" "CMakeFiles/gameEngine.dir/game/board/MoveSelector.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/board/MoveSelector.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/board/boardAnim/GameBoardAnim.cpp" "CMakeFiles/gameEngine.dir/game/board/boardAnim/GameBoardAnim.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/board/boardAnim/GameBoardAnim.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/logic/GameLogic.cpp" "CMakeFiles/gameEngine.dir/game/logic/GameLogic.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/logic/GameLogic.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/movesPlayersId/MovePlayersId.cpp" "CMakeFiles/gameEngine.dir/game/movesPlayersId/MovePlayersId.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/movesPlayersId/MovePlayersId.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/panels/PiecePromotionPanel.cpp" "CMakeFiles/gameEngine.dir/game/panels/PiecePromotionPanel.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/panels/PiecePromotionPanel.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/panels/buttons/PiecePromotionButton.cpp" "CMakeFiles/gameEngine.dir/game/panels/buttons/PiecePromotionButton.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/panels/buttons/PiecePromotionButton.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/PieceHandler.cpp" "CMakeFiles/gameEngine.dir/game/pieces/PieceHandler.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/PieceHandler.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/PieceHandlerPopulator.cpp" "CMakeFiles/gameEngine.dir/game/pieces/PieceHandlerPopulator.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/PieceHandlerPopulator.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/types/Bishop.cpp" "CMakeFiles/gameEngine.dir/game/pieces/types/Bishop.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/types/Bishop.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/types/ChessPiece.cpp" "CMakeFiles/gameEngine.dir/game/pieces/types/ChessPiece.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/types/ChessPiece.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/types/King.cpp" "CMakeFiles/gameEngine.dir/game/pieces/types/King.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/types/King.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/types/Knight.cpp" "CMakeFiles/gameEngine.dir/game/pieces/types/Knight.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/types/Knight.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/types/Pawn.cpp" "CMakeFiles/gameEngine.dir/game/pieces/types/Pawn.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/types/Pawn.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/types/Queen.cpp" "CMakeFiles/gameEngine.dir/game/pieces/types/Queen.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/types/Queen.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/types/Rook.cpp" "CMakeFiles/gameEngine.dir/game/pieces/types/Rook.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/types/Rook.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/pieces/types/UnfinishedPiece.cpp" "CMakeFiles/gameEngine.dir/game/pieces/types/UnfinishedPiece.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/pieces/types/UnfinishedPiece.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/game/utils/BoardUtils.cpp" "CMakeFiles/gameEngine.dir/game/utils/BoardUtils.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/game/utils/BoardUtils.cpp.obj.d"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/main.cpp" "CMakeFiles/gameEngine.dir/main.cpp.obj" "gcc" "CMakeFiles/gameEngine.dir/main.cpp.obj.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../manager/include"
+  "../sdl/include"
+  "../utils/include"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/build/manager/CMakeFiles/manager.dir/DependInfo.cmake"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/build/sdl/CMakeFiles/sdl.dir/DependInfo.cmake"
-  "C:/Users/user/GitHub Repositorys/GameEngine/Game-Engine/build/utils/CMakeFiles/utils.dir/DependInfo.cmake"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/manager/CMakeFiles/manager.dir/DependInfo.cmake"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/sdl/CMakeFiles/sdl.dir/DependInfo.cmake"
+  "/home/svetoslav/Documents/GitHub/GameEngine/Game-Engine/build/utils/CMakeFiles/utils.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
